0eae40271de4aeff5465feab49c552715b05ac09
詹其侁 <kkeevin123456@gmail.com>
2019-10-03 23:37:34 +0800

    T1 做好了

--- DC_project1.cpp
+++ DC_project1.cpp
@@ -2,6 +2,10 @@
 #include <fstream>
 using namespace std;
 
+// gobal variable
+int m, n;
+int map[20][41] = {0};
+
 int block_T1[4][3] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0};                  // num = 1
 int block_T2[4][3] = {0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0};                  // num = 2
 int block_T3[4][3] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1};                  // num = 3
@@ -26,10 +30,10 @@ class Block
 {
 private:
     /* data */
-    int** block_type;
     int col; // which col falling down 
 
 public:
+    int** block_type;
     void display(){
         if (sizeof(block_type) == 12) {
             for (int i=0; i<4; i++){
@@ -233,19 +237,138 @@ Block::~Block()
     delete [] block_type;
 }
 
+class dot
+{
+public:
+    int i, j, live;
+    dot(int row, int col, int live);
+};
 
-int main(){
-    int m, n, start_col, num;
-    string type_name;
-    cin >> m >> n;
+dot::dot(int row, int col, int live)
+{
+    i = row;
+    j = col;
+    this->live = live;
+}
 
-    int map[m+1][n+1];
-    // set map
-    for (int i=0; i<m; i++) {
-        for (int j=0; j<n; j++) {
-            map[i][j] = 0;
+int update_map(int num, int** piece, int col){
+    // check legal or not
+    if (num > 0 && num <17) {
+        for (int i=0; i<4; i++) {
+            for (int j=0; j<3; j++) {
+                if (piece[i][j] == 1) {
+                    if ((j+col) > n) return 0;  // error : over bounded
+                }
+            }
+        }
+    } else {
+        for (int i=0; i<4; i++) {
+            for (int j=0; j<4; j++) {
+                if (piece[i][j] == 1) {
+                    if ((j+col) > n) return 0;  // error : over bounded
+                }
+            }
         }
     }
+    
+    // put a piece into the top of map
+
+    if (num == 1) { // T1
+        map[3][col] = 1;
+        dot a(3, col, 1);
+
+        map[3][col+1] = 1;
+        dot b(3, col+1, 1);
+
+        map[3][col+2] = 1;
+        dot c(3, col+2, 1);
+
+        map[4][col+1] = 1;
+        dot d(4, col+1, 1);
+
+        // falling down
+        int stop = 0;
+        while(!stop){
+            if ( (d.i+1) > (m+4) ) stop = 1;
+            else if (a.live == 1 && b.live == 1 && c.live == 1 && d.live == 1 && (d.i+1) <= (m+4) ){ // T1丁字型
+                if (map[d.i+1][d.j] == 0 && map[a.i+1][a.j] == 0 && map[c.i+1][c.j] == 0) { // Ｔ１只要判斷ａ,ｄ,ｃ下面有沒有方塊就好
+                    // a
+                    map[a.i][a.j] = 0;
+                    a.i++;
+                    map[a.i][a.j] = 1;
+                    // b
+                    map[b.i][b.j] = 0;
+                    b.i++;
+                    // c
+                    map[c.i][c.j] = 0;
+                    c.i++;
+                    map[c.i][c.j] = 1;
+                    // d
+                    d.i++;
+                    map[d.i][d.j] = 1;
+                }
+                else stop = 1;
+            }
+        }
+        //看是否可以消除
+        int clear = 1;
+        for (int j=1; j<=n; j++) {
+            if (map[d.i][j] == 0) clear = 0;
+        }
+        if (clear) {
+            d.live = 0;
+            for (int j=1; j<=n; j++) { // clear row --> d.i
+                map[d.i][j] = 0;
+            }
+
+            for (int i = d.i-1; i>4; i--) { // 上面全部都往下移
+                for (int j=1; j<=n; j++) {
+                    map[i+1][j] = map[i][j];
+                    map[i][j] = 0;
+                }
+            }
+            a.i++; b.i++; c.i++;
+        }
+        // 判斷ａｂｃ那行可不可消
+        clear = 1;
+        for (int j=1; j<=n; j++) {
+            if (map[a.i][j] == 0) clear = 0;
+        }            
+        if (clear) {
+            for (int j=1; j<=n; j++) {
+                map[a.i][j] = 0;
+            }
+
+            for (int i = a.i-1; i>4; i--) {
+                for (int j=1; j<=n; j++) {
+                    map[i+1][j] = map[i][j];
+                    map[i][j] = 0;
+                }
+            }
+        }
+        //判斷是不是輸掉 看上面那塊區域還有沒有1
+        for(int i=1; i<5; i++) {
+            for (int j=1; j<=n; j++) {
+                if(map[i][j]==1) return 1;
+            }
+        }         
+        return 0; /// done!!!!
+    }
+    else if (num == 2) { // T2
+
+    }
+    else if (num == 3) { // T3
+
+    }
+    return 0;
+
+}
+
+
+int main(){
+    int start_col, num;
+    string type_name;
+    cin >> m >> n;
 
     while(1){
         // input block
@@ -300,7 +423,21 @@ int main(){
 
 
         // update map
-        
+        int gameover = update_map(num, b.block_type, start_col);
+
+        // debug : display map
+        for (int i=5; i<=m+4; i++) {
+            for (int j=1; j<=n; j++) {
+                cout << map[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        // game over
+        if (gameover) {
+            cout << "Game Over!" << endl;
+            break;
+        }
 
     }
 }
